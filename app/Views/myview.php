c1b9466de2f3d39e02e0dca7b50cf3513b17be5d
skmirajbn <skmirajbn@gmail.com>
2023-09-21 20:33:19 +0600

    updated

--- app/Views/myview.php
+++ app/Views/myview.php
@@ -4,11 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to my project</title>
+    <title>
+        <?php echo $page_title ?>
+    </title>
 </head>
 
 <body>
-    <h1>Welcome to my project</h1>
+    <h1>
+        <?= $page_heading ?>
+    </h1>
 </body>
 
 </html>
+
+Templeting Engine
