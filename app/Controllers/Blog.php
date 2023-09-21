b5eb9cde43a00fa13692f30ab1bd7265f45a1bd2
skmirajbn <skmirajbn@gmail.com>
2023-09-21 20:30:43 +0600

    updated

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -5,8 +5,12 @@ use CodeIgniter\Controller;
 
 class Blog extends Controller {
     public function getIndex() {
+        $data = [
+            "page_title" => "CodeIgnitor 4",
+            "page_heading" => "CodeIgniter 4 views"
+        ];
         echo view('header');
-        echo view('myview');
+        echo view('myview', $data);
         echo view('footer');
     }
 }
