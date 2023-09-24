ac762fa1421e15ffc3909f4489552ca16b6fcfe6
Sk Miraj <skmirajbn@gmail.com>
2023-09-24 10:43:02 +0600

    updated

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
-{
-    public function index(): string
-    {
-        return view('welcome_message');
+use CodeIgniter\Controller;
+
+class Home extends Controller {
+    public function index(): string {
+        return view('index');
     }
 }
---
+++ app/Views/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>This is My Website</title>
+    <link rel="stylesheet" href="<?= base_url() ?>public/css/output.css">
+</head>
+
+<body>
+    <div class="bg-gray-200 text-black py-3">
+        <header class="container flex justify-between items-center">
+            <div><img class="w-8" src="<?= base_url() ?>public/img/logo.svg" alt=""></div>
+            <nav class="space-x-8 text-bold">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Social</a>
+                <a href="#">Career</a>
+                <a href="#">Contact</a>
+            </nav>
+            <div>
+                <input class="rounded-full px-4 py-1 text-black" type="text" placeholder="Search">
+            </div>
+        </header>
+    </div>
+</body>
+
+</html>
