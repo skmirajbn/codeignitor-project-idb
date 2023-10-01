b5253a04293655437a48a4921e43d1a211a22633
Sk Miraj <skmirajbn@gmail.com>
2023-10-01 13:13:25 +0600

    updated

--- next-social-media/src/components/comment.jsx
+++ next-social-media/src/components/comment.jsx
@@ -1,5 +1,16 @@
 function Comment() {
-  return <div>Comment</div>;
+  return (
+    <div>
+      <div>
+        <div>Profile Image</div>
+        <h3>Comment Text</h3>
+        <h5>Like</h5>
+      </div>
+      <div>
+        <input className="border-gray-500 border-2 w-full" type="text" placeholder="Enter Comment Here" />
+      </div>
+    </div>
+  );
 }
 
 export default Comment;
--- next-social-media/src/components/post.jsx
+++ next-social-media/src/components/post.jsx
@@ -4,8 +4,16 @@ export default function Post() {
       <h3 className="text-3xl">Here is the post text</h3>
       <img className="rounded-lg w-full" src="img/post-image.webp" alt="" />
       {/* Like Comment Share Section */}
-      <div>
-        <i class="fa-solid fa-thumbs-up"></i> Like
+      <div className="flex justify-between text-lg">
+        <div>
+          <i class="fa-solid fa-thumbs-up"></i> 2670 Like
+        </div>
+        <div>
+          <i class="fa-solid fa-comment"></i> 750 Comment
+        </div>
+        <div>
+          <i class="fa-solid fa-share"></i> 150 Share
+        </div>
       </div>
     </div>
   );
