31e513994672a87155e0229b6a0f9530098b38b8
Sk Miraj <skmirajbn@gmail.com>
2023-10-02 12:03:26 +0600

    updated

--- next-social-media/src/components/header.jsx
+++ next-social-media/src/components/header.jsx
@@ -1,10 +1,16 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useRef } from "react";
 import logo from "./../../public/img/logo.png";
 import profile from "./../../public/img/profile.jpg";
 
 export default function Header() {
+  const messageModal = useRef();
+  const showToggle = () => {
+    messageModal.current.classList.toggle("hidden");
+    console.log("message clicked");
+  };
   return (
     <header class=" flex justify-between items-center bg-white text-black py-3 px-5 shadow-lg sticky top-0 z-10">
       <Link href="/" class="flex space-x-4 w-1/4">
@@ -36,9 +42,106 @@ export default function Header() {
             </a>
           </div>
         </div>
-        <a class="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full relative" onClick={() => document.getElementById("my_modal_2").showModal()}>
-          <i class="fa-brands fa-facebook-messenger "></i>
+        <a class="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full relative">
+          <i class="fa-brands fa-facebook-messenger" onClick={showToggle}></i>
           <div className="bg-red-600 h-5 w-5 rounded-full absolute flex justify-center items-center text-xs text-white font-medium -right-1 -top-1">15</div>
+
+          {/* Modal */}
+          <div ref={messageModal} className="absolute bg-white top-14 -right-32 px-6 w-80 py-6 min-h-[80vh] shadow-lg shadow-gray-700 rounded-md hidden overflow-auto h-[90vh]">
+            <i class="text-white text-5xl fa-solid fa-sort-up absolute -top-4 left-1/2 -translate-x-1/2"></i>
+            <div className="space-y-6">
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+              {/* Single Message */}
+              <div className="space-y-2">
+                <div className="flex gap-3 items-center">
+                  <img className="w-8 h-8 rounded-full object-cover" src="img/profile.jpg" alt="" />
+                  <h3 className="text-sm">Sheikh Rownakul Islam Miraj</h3>
+                  <i class="fa-solid fa-circle text-green-600 text-sm"></i>
+                </div>
+                <h5 className="text-xs">Hello How are you?</h5>
+                <hr />
+              </div>
+            </div>
+          </div>
         </a>
         <a href="#" class="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full relative">
           <i class="fa-solid fa-bell "></i>
@@ -48,19 +151,6 @@ export default function Header() {
           <Image class="w-12 h-12 rounded-full overflow-hidden object-cover" src={profile} alt="" />
         </a>
       </div>
-
-      {/* Messageing Modal */}
-
-      {/* Open the modal using document.getElementById('ID').showModal() method */}
-      <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Messages!</h3>
-          <p className="py-4">Press ESC key or click outside to close</p>
-        </div>
-        <form method="dialog" className="modal-backdrop">
-          <button>close</button>
-        </form>
-      </dialog>
     </header>
   );
 }
