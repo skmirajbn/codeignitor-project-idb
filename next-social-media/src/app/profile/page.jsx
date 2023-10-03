db5069f43f407fb69e348db72a8cd32d7b645292
skmirajbn <skmirajbn@gmail.com>
2023-10-04 01:47:08 +0600

    updated

--- next-social-media/src/app/profile/page.jsx
+++ next-social-media/src/app/profile/page.jsx
@@ -7,7 +7,14 @@ export default function Profile() {
       <Header />
       <div className="container">
         <img className="h-[30rem] w-full object-cover rounded-lg" src="img/cover.jpg" alt="" />
-        <img className="h-36 w-36 object-cover rounded-full" src="img/profile.jpg" alt="" />
+        <div className="flex gap-6 items-end -mt-20 px-10">
+          <img className="h-44 w-44 object-cover rounded-full" src="img/profile.jpg" alt="" />
+          <div>
+            <h2 className="text-4xl font-bold">Sheikh rownakul Islam Miraj</h2>
+            <h5>64 Friends</h5>
+          </div>
+        </div>
+        <hr className="mt-3" />
       </div>
     </div>
   );
