fd386ec8053b8154a112d3bd3e9c7a2154c86028
skmirajbn <skmirajbn@gmail.com>
2023-10-03 10:14:14 +0600

    updated

--- next-social-media/src/app/messages/page.jsx
+++ next-social-media/src/app/messages/page.jsx
@@ -2,9 +2,144 @@ import Header from "@/components/header";
 
 export default function Page() {
   return (
-    <div>
+    <div className="h-screen">
       <Header />
-      This is messages page
+      <section className="flex">
+        {/* Message Sidebar */}
+        <div className="w-1/4 py-4 px-6 space-y-3 h-full">
+          <div className=" bg-white  flex justify-between items-center">
+            <h2 className="text-2xl font-bold">Chats</h2>
+            <div className="flex gap-4">
+              <div className="bg-gray-300 h-10 w-10 flex justify-center items-center rounded-full">
+                <i class="fa-solid fa-ellipsis text-xl"></i>
+              </div>
+              <div className="bg-gray-300 h-10 w-10 flex justify-center items-center rounded-full">
+                <i class="fa-solid fa-pen-to-square text-xl"></i>
+              </div>
+            </div>
+          </div>
+          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs rounded-full bg-gray-100 h-[2rem]" />
+          <h3 className="text-xl font-bold">Inbox</h3>
+          <div className="overflow-auto space-y-3 h-[70vh]">
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+            {/* single Message Sidebar */}
+            <div className="flex gap-3 items-center">
+              <img className="h-10 w-10 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <div>
+                <h3>Jahangir Alam</h3>
+                <p>Hello How are you?</p>
+              </div>
+              <i class="fa-solid fa-comment text-blue-600"></i>
+            </div>
+          </div>
+        </div>
+        {/* Message Body */}
+        <div className="w-2/4 p-4 space-y-3">
+          <div className="flex items-center gap-4 justify-between">
+            <div className="flex items-center gap-4">
+              <img className="h-14 w-14 object-cover rounded-full" src="img/profile.jpg" alt="" />
+              <h3 className="text-xl font-bold">Mr. Mokhles Uddin</h3>
+            </div>
+            <div className="flex gap-4 text-blue-600">
+              <i class="fa-solid fa-phone text-xl"></i>
+              <i class="fa-solid fa-video text-xl"></i>
+              <i class="fa-solid fa-circle-info text-xl"></i>
+            </div>
+          </div>
+          <hr />
+        </div>
+        {/* Message options */}
+        <div className="w-1/4 "></div>
+      </section>
     </div>
   );
 }
--- next-social-media/src/components/header.jsx
+++ next-social-media/src/components/header.jsx
@@ -13,10 +13,12 @@ export default function Header() {
   };
   return (
     <header class=" flex justify-between items-center bg-white text-black py-3 px-5 shadow-lg sticky top-0 z-10">
-      <Link href="/" class="flex space-x-4 w-1/4">
-        <Image class="w-10" src={logo} alt="" />
+      <div class="flex space-x-4 w-1/4">
+        <Link href="/">
+          <Image class="w-10" src={logo} alt="" />
+        </Link>
         <input class="w-44 rounded-full px-4 py-1 text-black bg-gray-200" type="text" placeholder="Search" />
-      </Link>
+      </div>
       <nav class="flex space-x-16 text-bold justify-center w-2/4 text-2xl">
         <a class="text-blue-600" href="#">
           <i class="fa-solid fa-house"></i>
@@ -42,7 +44,7 @@ export default function Header() {
             </a>
           </div>
         </div>
-        <a class="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full relative">
+        <div class="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full relative">
           <i class="fa-brands fa-facebook-messenger" onClick={showToggle}></i>
           <div className="bg-red-600 h-5 w-5 rounded-full absolute flex justify-center items-center text-xs text-white font-medium -right-1 -top-1">15</div>
 
@@ -145,7 +147,7 @@ export default function Header() {
               See All in Messenger
             </Link>
           </div>
-        </a>
+        </div>
         <a href="#" class="bg-gray-200 w-10 h-10 flex justify-center items-center rounded-full relative">
           <i class="fa-solid fa-bell "></i>
           <div className="bg-red-600 h-5 w-5 rounded-full absolute flex justify-center items-center text-xs text-white font-medium -right-1 -top-1">28</div>
