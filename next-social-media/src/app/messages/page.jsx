19609f74b104df9515289c3c6605a294f3f65bfb
skmirajbn <skmirajbn@gmail.com>
2023-10-04 01:29:16 +0600

    updated

--- next-social-media/src/app/messages/page.jsx
+++ next-social-media/src/app/messages/page.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Header from "@/components/header";
 import MessageBody from "./messageBody";
+import MessageOptions from "./messageOptions";
 import MessageSidebar from "./messageSidebar";
 
 export default function Page() {
@@ -10,25 +11,7 @@ export default function Page() {
       <section className="flex flex-1">
         <MessageSidebar />
         <MessageBody />
-        {/* Message options */}
-        <div className="w-1/4 py-4 space-y-4">
-          <img className="h-32 w-32 object-cover rounded-full mx-auto" src="img/profile.jpg" alt="" />
-          <h3 className="text-2xl text-center font-bold">Mr. Mokhles Uddin</h3>
-          <h5 className="text-center">
-            <i class="fa-solid fa-circle text-green-600"></i> Online
-          </h5>
-          <div className="flex justify-center gap-6 text-xl">
-            <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
-              <i class="fa-solid fa-user"></i>
-            </div>
-            <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
-              <i class="fa-solid fa-volume-xmark"></i>
-            </div>
-            <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </div>
-          </div>
-        </div>
+        <MessageOptions />
       </section>
     </div>
   );
